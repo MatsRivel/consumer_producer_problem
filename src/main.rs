7b7e6084f2168397e46ed29c2c54a6bc6ea70bea
Mats Riveland <mats-r@hotmail.com>
2023-03-23 12:22:41 +0100

    Produser Consumer problem working

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use std::sync::{Arc,Mutex, Condvar};
 struct BoundedQueue<T> {
     queue: VecDeque<T>,
     capacity: usize,
-
 }
 
 impl<T> BoundedQueue<T> {
@@ -19,30 +18,31 @@ impl<T> BoundedQueue<T> {
         }
     }
 
-    fn push(&mut self, element: T) -> Result<(), &str> {
+    fn push(&mut self, element: T) -> Option<()> {
         // Push only if there is room.
-        if self.queue.len() < self.capacity {
-            self.queue.push_back(element);
-            return Ok(());
-        }
-        return Err("Pushed to full queue");
+        match self.is_full(){
+            false => {
+                self.queue.push_back(element);
+                Some(())
+            }
+            true => None,
+        } // No ";", so it returns the output of the match.
     }
 
     fn pop(&mut self) -> Option<T> {
-        return self.queue.pop_front();
+        self.queue.pop_front()
     }
-
+    #[allow(dead_code)] // TODO: Remove
     fn is_empty(&mut self) -> bool{
-        return self.queue.is_empty()
+        self.queue.is_empty()
     }
-
     fn is_full(&mut self) -> bool{
-        return self.queue.len() == self.capacity;
+        self.queue.len() == self.capacity
     }
 }
 
 const N_PRODUCTION: i32 = 10;
-const N_CAPACITY: usize = 3;
+const N_CAPACITY: usize = 9;
 fn main() {
     // Goal: To implement the solution to the consumer - producer problem.
     // Queue can hold up to 10 elements at a time.
@@ -54,83 +54,65 @@ fn main() {
     // Producer and consumer should run in separate threads.
     let queue = Arc::new((Mutex::new(BoundedQueue::<i32>::new(N_CAPACITY)), Condvar::new(), Condvar::new()) );
     let push_queue = queue.clone();
+    let pull_queue = queue.clone();
     
     let push_handle = thread::spawn(move || {
-        //print!("Producer started!\n");
         let mut i = 0;
         while i < N_PRODUCTION {
-            sleep(Duration::from_millis(1)); // TODO: REMOVE
-            println!("i: {}",i);
-            // Try to access: If yes, propagate value to push. Else, reset loop.
-            let mut push_lock_result = match push_queue.0.lock(){
-                Ok(v) => v,
-                Err(e) => {
-                    println!(">>> {} <<<", e);
-                    continue;
-                },
+            let mut guard = match push_queue.0.lock(){
+                Ok(v) => v,                            // Lock succeeded, keep going.
+                Err(s) => panic!("{}",s),   // TODO: Handle error better.
             };
-
-
-
-            // Try to push element. If queue has room, push and continue. Else reset loop.
-            match push_lock_result.push(i){
-                Ok(_) => {
-                    i+=1;
-                    push_queue.2.notify_all();
-                },
-                Err(_s) => {
-                    print!(">>> i: {}|{} <<<\n", i, _s);
-                    push_queue.2.notify_all();
-                    match push_queue.1.wait_while(push_queue.0.lock().unwrap(), |push_q: &mut BoundedQueue<i32>| push_q.is_full()){
-                        Ok(_) => {}, // Run as normal.
-                        Err(_) => continue, // Skip to next iter.
-                    }
-                    continue;
-                },
+            
+            match guard.push(i){
+                Some(_) => {                             // Push successfull. 
+                            push_queue.2.notify_one();   // Notify pull that at least one element is available.
+                            println!("Pushed {}",i);
+                            i+=1;
+                            continue;                    // Jump to the start of the loop again.
+                        },   
+                None => {},                              // Pushing to full queue. Moving on to wait section.
+            }
+            push_queue.2.notify_one();           // Notify pull that at least one element is available.
+            
+            println!("Push is waiting...");
+            match push_queue.1.wait(guard){      // Waiting for notification of available space.
+                Ok(_) => {},                     // No issue, go to beginning of loop.           
+                Err(s) => panic!("{}",s),   // TODO: Handle error better.
             }
         }
+        push_queue.2.notify_all();
+        
     });
 
-    let pull_queue = queue.clone();
     let pull_handle = thread::spawn(move || {
-        //print!("Consumer started!\n");
-        let mut j = 0;
-        while j < N_PRODUCTION {
-            sleep(Duration::from_millis(10)); // TODO: REMOVE
-            // Try to access: If yes, propagate value to push. Else, reset loop.
-            println!("j: {}",j);
-            let mut pull_lock_result= match pull_queue.0.lock(){
-                Ok(v) => v,
-                Err(e) => { // Tried to access locked mutex
-                    println!(">>> {} <<<", e);
-                    continue;
-                },
+        let mut i = 0;
+        while i < N_PRODUCTION {
+            let mut guard = match pull_queue.0.lock(){
+                Ok(v) => v,                            // Lock succeeded, keep going.
+                Err(s) => panic!("{}",s),   // TODO: Handle error better.
             };
-
-            // Try to push element. If queue has room, push and continue. Else reset loop.
-            match pull_lock_result.pop(){
-                Some(_) => {
-                    j+=1;
-                    pull_queue.1.notify_all();
-                },
-                None => { // Tried to pop from empty queue.
-                    println!(">>> j: {}| Pulled from empty queue. <<<", j);
-                    pull_queue.1.notify_all();
-                    match pull_queue.2.wait_while(pull_queue.0.lock().unwrap(), |push_q: &mut BoundedQueue<i32>| push_q.is_empty()){
-                        Ok(_) => {}, // Run as normal.
-                        Err(_) => continue, // Skip to next iter.
-                    }
-                    continue;},
+            
+            match guard.pop(){
+                Some(v) => {                        // Pull successfull. 
+                            pull_queue.1.notify_one();   // Notify push that at least one space is available.
+                            println!("\t\tPulled {}",v);
+                            i+=1;
+                            continue;
+                        },   
+                None => {},                              // Pulling from empty queue. Moves on to wait section
+            }
+            pull_queue.1.notify_one();           // Notify push that at least one space is available.
+            
+            println!("Pull is waiting...");
+            match pull_queue.2.wait(guard){      // Waiting for notification of available space.
+                Ok(_) => {},                     // No issue, go to beginning of loop.                                            
+                Err(s) => panic!("{}",s),   // TODO: Handle error better.
             }
         }
-    });
+        pull_queue.1.notify_all();
 
-    match push_handle.join() {
-        Ok(_) => {
-                //print!("\nProducer finished!\n");
-                },
-        Err(_) => print!("\n>>> Producer panicked! <<<\n"),
-    }
+    });
 
 
     match pull_handle.join() {
@@ -138,6 +120,12 @@ fn main() {
                 //print!("Consumer finished!\n");
                 },
         Err(_) => print!(">>> Consumer panicked! <<<\n"),
+    }
+    match push_handle.join() {
+        Ok(_) => {
+                //print!("\nProducer finished!\n");
+                },
+        Err(_) => print!("\n>>> Producer panicked! <<<\n"),
     }
     let final_size = queue.0.lock().unwrap().queue.len(); 
     match final_size{
